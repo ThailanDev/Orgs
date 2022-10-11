8f0c3d90f812a9e3eda37b256745c04f48bb54d6
Thailan Godoy <thailanrg@gmail.com>
2022-10-10 22:20:34 -0300

    filtro

--- app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
+++ app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
@@ -9,6 +9,24 @@ interface ProdutoDao {
     @Query("SELECT * FROM Produto")
     fun buscaTodos(): List<Produto>
 
+    @Query("SELECT * FROM Produto ORDER BY nome DESC")
+    fun buscarPorNomeDesc(): List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY nome ASC")
+    fun buscarPorNomeAsc(): List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY descricao DESC")
+    fun buscarPorDescricaoDesc(): List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY descricao ASC")
+    fun buscarPorDescricaoAsc(): List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY valor DESC")
+    fun buscarPorValorDesc(): List<Produto>
+
+    @Query("SELECT * FROM Produto ORDER BY valor ASC")
+    fun buscarPorValorAsc(): List<Produto>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun salva(vararg produto: Produto)
 
--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -3,7 +3,11 @@ package br.com.alura.orgs.ui.activity
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.orgs.R
 import br.com.alura.orgs.database.AppDatabase
 import br.com.alura.orgs.databinding.ActivityListaProdutosActivityBinding
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
@@ -15,6 +19,9 @@ class ListaProdutosActivity : AppCompatActivity() {
         ActivityListaProdutosActivityBinding.inflate(layoutInflater)
     }
     private lateinit var db: AppDatabase
+    private val produtoDao by lazy {
+        AppDatabase.instancia(this).produtoDao()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +32,39 @@ class ListaProdutosActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        db = AppDatabase.instancia(this)
-        val produtoDao = db.produtoDao()
         adapter.atualiza(produtoDao.buscaTodos())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_ordenacao, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nomeDesc -> {
+                adapter.atualiza(produtoDao.buscarPorNomeDesc())
+            }
+            R.id.nomeAsc -> {
+                adapter.atualiza(produtoDao.buscarPorNomeAsc())
+            }
+            R.id.descricaoDesc -> {
+                adapter.atualiza(produtoDao.buscarPorDescricaoDesc())
+            }
+            R.id.descricaoAsc -> {
+                adapter.atualiza(produtoDao.buscarPorDescricaoAsc())
+            }
+            R.id.valorDesc -> {
+                adapter.atualiza(produtoDao.buscarPorValorDesc())
+            }
+            R.id.valorAsc -> {
+                adapter.atualiza(produtoDao.buscarPorValorAsc())
+            }
+            R.id.semOrdenacao -> {
+                adapter.atualiza(produtoDao.buscaTodos())
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun configuraFab() {
